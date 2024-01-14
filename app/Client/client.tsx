a2333ad02544a1270dd953cd38e9379eae604869
SURAJ KADAM <surajkadam381@gmail.com>
2024-01-15 00:45:35 +0530

    modified backed connection

--- app/Client/client.tsx
+++ app/Client/client.tsx
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-    projectId: process.env.NEXT_APP_SANITY_PROJECT_ID,
+    projectId: "1p7yzy7v",
     dataset: "production",
     apiVersion: "2022-03-07",
     useCdn: true
