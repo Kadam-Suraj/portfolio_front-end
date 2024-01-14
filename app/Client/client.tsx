74a0b7065b2862d39e5d0cd47b88f94128844e6f
SURAJ KADAM <surajkadam381@gmail.com>
2024-01-14 23:08:49 +0530

    second commit

--- app/Client/client.tsx
+++ app/Client/client.tsx
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-    projectId: "1p7yzy7v",
+    projectId: process.env.NEXT_APP_SANITY_PROJECT_ID,
     dataset: "production",
     apiVersion: "2022-03-07",
     useCdn: true
