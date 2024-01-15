23a9c786234ee5723cac3cfd914b28ca095812aa
SURAJ KADAM <115386696+SK170401@users.noreply.github.com>
2024-01-15 11:54:01 +0530

    Style: Update Education.tsx

--- app/Container/Education/Education.tsx
+++ app/Container/Education/Education.tsx
@@ -16,7 +16,7 @@ const Education = async () => {
             <div>
                 <h2 className='text-transparent bg-clip-text bg-gradient-to-r from-[#B16CEA] via-[#FF5E69] to-[#FFA84B] w-fit text-[3em] font-bold pb-14'>Education</h2>
                 <div>{data.map((item, idx) => {
-                    return <div key={idx} className='flex flex-col gap-5 justify-center border-b pb-5 border-gray-400'>
+                    return <div key={idx} className='flex flex-col gap-5 justify-center border-b py-5 border-gray-400'>
                         <h4 className='font-semibold'>{item.university}</h4>
                         <div className='flex justify-between items-center gap-8'>
                             <span className='font-light text-opacity-80 text-white'>{item.degree}</span>
