37898cfc56ac372b04daae8beeb3b15771794fc2
SK170401 <surajkadam381@gmail.com>
2024-02-08 00:22:07 +0530

    Fixes

--- app/Projects/[...slug]/page.tsx
+++ app/Projects/[...slug]/page.tsx
@@ -31,7 +31,7 @@ const slug = async ({ params }) => {
                     return <div key={idx}>
                         <div className='flex max-[880px]:flex-col justify-between items-center gap-10'>
                             <div className='lg:w-1/2 h-full flex justify-center'>
-                                <Image className='rounded-xl pointer-events-none' src={urlFor(item.image).url()} alt={item.caption} width={500} height={500} draggable={false}></Image>
+                                <Image className='rounded-xl pointer-events-none' src={urlFor(item.image).url()} alt={item.tag} width={500} height={500} draggable={false}></Image>
                             </div>
                             <div className='flex flex-col w-full min-[880px]:w-1/2 max-sm:px-0 max-[880px]:px-14 self-start'>
                                 <h1 className='text-[3em] font-bold text-transparent bg-clip-text w-fit bg-gradient-to-r from-[#B16CEA] via-[#FF5E69] to-[#FFA84B]'>{item.name}</h1>
