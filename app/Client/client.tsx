3fd2dd6ba8a3fa2628a8d8d95cb1a2b6c0682462
Kadam-Suraj <surajkadam381@gmail.com>
2025-01-02 20:36:24 +0530

    fix: set default projectId to a specific value for improved stability

--- app/Client/client.tsx
+++ app/Client/client.tsx
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || undefined,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "1p7yzy7v",
     dataset: "production",
     apiVersion: "2022-03-07",
     useCdn: true
