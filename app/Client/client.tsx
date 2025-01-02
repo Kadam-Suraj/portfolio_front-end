86920b92c0288ab8724e52ace94b565a7ecb5a4e
Kadam-Suraj <surajkadam381@gmail.com>
2025-01-02 20:34:35 +0530

    fix: update projectId fallback to undefined for better error handling

--- app/Client/client.tsx
+++ app/Client/client.tsx
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || undefined,
     dataset: "production",
     apiVersion: "2022-03-07",
     useCdn: true
